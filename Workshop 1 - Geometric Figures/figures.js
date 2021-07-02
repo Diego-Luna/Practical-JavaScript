738cffeaebbd4af5b1055a4ec4217bffdf37be13
Dieg Luna <diegofcolunalopez@gmail.com>
2021-07-02 16:49:19 -0500

    Adding styles to Workshop # 1

--- Workshop 1 - Geometric Figures/figures.js
+++ Workshop 1 - Geometric Figures/figures.js
@@ -15,7 +15,11 @@ console.groupEnd();
 console.group("Triangle");
 
 function trianglePerimeter(side1, side2, base) {
-  return side1 + side2 + base;
+  const Side1 = parseInt(side1);
+  const Side2 = parseInt(side2);
+  const Base = parseInt(base);
+
+  return Side1 + Side2 + Base;
 }
 
 function triangleArea(base, height) {
@@ -45,7 +49,6 @@ function circleArea(radius) {
 
 console.groupEnd();
 
-
 function calculatePerimeterSquare() {
   const input = document.getElementById("InputSquare");
   const value = input.value;
@@ -59,4 +62,48 @@ function calculateAreaSquare() {
 
   const perimeter = squareArea(value);
   alert(perimeter);
+}
+
+function calculatePerimeterCircle() {
+  const input = document.getElementById("InputCircle");
+  const value = input.value;
+
+  const perimeter = circlePerimeter(value);
+  alert(perimeter);
+}
+function calculateAreaCircle() {
+  const input = document.getElementById("InputCircle");
+  const value = input.value;
+
+  const perimeter = circleArea(value);
+  alert(perimeter);
+}
+
+function calculatePerimeterTriangle() {
+  // const input = document.getElementById("InputCircle");
+  // const value = input.value;
+
+  const inputTriangleSide1 = document.getElementById("InputTriangleSide1");
+  const inputTriangleSide2 = document.getElementById("InputTriangleSide2");
+  const inputTriangleBase = document.getElementById("InputTriangleBase");
+
+  const valueTriangleSide1 = inputTriangleSide1.value;
+  const valueTriangleSide2 = inputTriangleSide2.value;
+  const valueTriangleBase = inputTriangleBase.value;
+
+  const perimeter = trianglePerimeter(
+    valueTriangleSide1,
+    valueTriangleSide2,
+    valueTriangleBase
+  );
+  alert(perimeter);
+}
+function calculateAreaTriangle() {
+  const inputTriangleHeight = document.getElementById(
+    "InputTriangleHeight"
+  ).value;
+  const inputTriangleBase = document.getElementById("InputTriangleBase").value;
+
+  const area = triangleArea(inputTriangleBase, inputTriangleHeight);
+  alert(area);
 }
